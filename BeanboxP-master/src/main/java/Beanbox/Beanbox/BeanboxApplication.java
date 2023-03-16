d67582a82347f740e0958b8b428d297d70322550
KKKJJJSSS <kj971023@naver.com>
2023-03-17 03:20:28 +0900

    1.2

---
+++ BeanboxP-master/src/main/java/Beanbox/Beanbox/BeanboxApplication.java
@@ -0,0 +1,13 @@
+package Beanbox.Beanbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanboxApplication.class, args);
+	}
+
+}
---
+++ BeanboxP-master/src/test/java/Beanbox/Beanbox/BeanboxApplicationTests.java
@@ -0,0 +1,13 @@
+package Beanbox.Beanbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
