b08026cc8e15a3de409101f87e201d0e5a0f6d55
KKKJJJSSS <kj971023@naver.com>
2023-04-05 17:01:36 +0900

    20230405

---
+++ BeanboxP-master/src/main/java/Beanbox/Beanbox/MainController.java
@@ -0,0 +1,48 @@
+package Beanbox.Beanbox;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/recipe")
+    public String recipe() {
+        return "recipe";
+    }
+    @GetMapping("/cart")
+    public String cart() {
+        return "cart";
+    }
+    @GetMapping("/gps")
+    public String gps() {
+        return "gps";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/menu")
+    public String menu() {
+        return "menu";
+    }
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "mypage";
+    }
+    @GetMapping("/order")
+    public String order() {
+        return "order";
+    }
+    @GetMapping("/sidebutton")
+    public String sidebutton() {
+        return "sidebutton";
+    }
+
+
+
+
+}
