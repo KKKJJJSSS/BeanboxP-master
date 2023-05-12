8845cdf8528d5ba280fab561177c03a78a134d48
KKKJJJSSS <kj971023@naver.com>
2023-05-12 14:53:50 +0900

    230512

--- BeanboxP-master/src/main/java/Beanbox/Beanbox/controller/MainController.java
+++ BeanboxP-master/src/main/java/Beanbox/Beanbox/controller/MainController.java
@@ -1,4 +1,4 @@
-package Beanbox.Beanbox.Controller;
+package Beanbox.Beanbox.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
