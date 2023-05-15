ed984ac18ad30e445c900d64d96dcb778ae7b774
KKKJJJSSS <kj971023@naver.com>
2023-05-15 14:39:05 +0900

    230515

--- BeanboxP-master/src/main/java/Beanbox/Beanbox/TextExtractor.java
+++ BeanboxP-master/src/main/java/Beanbox/Beanbox/TextExtractor.java
@@ -1,23 +1,16 @@
 package Beanbox.Beanbox;
 
-import com.google.cloud.vision.v1.Image;
 import com.google.cloud.vision.v1.*;
 import com.google.protobuf.ByteString;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,18 +25,11 @@ public class TextExtractor {
     @PostMapping("/extract")
     public String extractText(@RequestParam("file") MultipartFile file, Model model) throws IOException {
         try {
-            // 파일을 로컬 디스크에 저장
-            Path tempFile = Files.createTempFile("temp-", "-" + file.getOriginalFilename());
-            try (OutputStream os = Files.newOutputStream(tempFile)) {
-                os.write(file.getBytes());
-            }
+            // MultipartFile을 byte 배열로 변환
+            byte[] fileBytes = StreamUtils.copyToByteArray(file.getInputStream());
 
-            // 저장한 파일을 이용하여 Image 생성
-            BufferedImage img = ImageIO.read(tempFile.toFile());
-            BufferedImage resizedImg = resizeImage(img, 800, 600); // 이미지의 크기를 800x600으로 조정
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(resizedImg, "jpg", baos);
-            ByteString imgBytes = ByteString.copyFrom(baos.toByteArray());
+            // byte 배열을 이용하여 Image 생성
+            ByteString imgBytes = ByteString.copyFrom(fileBytes);
             Image image = Image.newBuilder().setContent(imgBytes).build();
 
             // API 호출을 위한 ImageAnnotatorClient 생성
@@ -61,18 +47,18 @@ public class TextExtractor {
                 requests.add(request);
 
                 // API 호출 및 응답 받기
-                List<AnnotateImageResponse> responses = client.batchAnnotateImages(requests).getResponsesList();
+                BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+                List<AnnotateImageResponse> responses = response.getResponsesList();
 
                 // 추출한 텍스트 가져오기
                 String extractedText = "";
-                for (AnnotateImageResponse response : responses) {
-                    if (response.hasError()) {
-                        System.err.println("Error: " + response.getError().getMessage());
+                for (AnnotateImageResponse annotateResponse : responses) {
+                    if (annotateResponse.hasError()) {
+                        System.err.println("Error: " + annotateResponse.getError().getMessage());
                         return "error"; // 오류 처리
                     }
-                    TextAnnotation textAnnotation = response.getFullTextAnnotation();
-                    if (textAnnotation != null) {
-                        extractedText += textAnnotation.getText();
+                    for (EntityAnnotation annotation : annotateResponse.getTextAnnotationsList()) {
+                        extractedText += annotation.getDescription();
                     }
                 }
 
@@ -81,20 +67,10 @@ public class TextExtractor {
 
             } // API 클라이언트가 자동으로 종료됩니다.
 
-            // 임시 파일 삭제
-            Files.deleteIfExists(tempFile);
-
             return "result"; // 결과 페이지로 이동
         } catch (IOException e) {
             e.printStackTrace();
             return "error";
         }
-    }
-    private BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) {
-        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
-        Graphics2D graphics2D = resizedImage.createGraphics();
-        graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
-        graphics2D.dispose();
-        return resizedImage;
     }
 }
