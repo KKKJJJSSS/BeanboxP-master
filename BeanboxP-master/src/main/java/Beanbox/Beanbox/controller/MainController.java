00273890e20bc4976e36c8c9691d13e192a3a6a3
KKKJJJSSS <kj971023@naver.com>
2023-05-26 03:37:40 +0900

    230526

--- BeanboxP-master/src/main/java/Beanbox/Beanbox/controller/MainController.java
+++ BeanboxP-master/src/main/java/Beanbox/Beanbox/controller/MainController.java
@@ -9,34 +9,49 @@ public class MainController {
     public String index() {
         return "index";
     }
+
     @GetMapping("/cart")
     public String cart() {
         return "cart";
     }
+
     @GetMapping("/gps")
     public String gps() {
         return "gps";
     }
+
     @GetMapping("/login")
     public String login() {
         return "login";
     }
+
     @GetMapping("/menu")
     public String menu() {
         return "menu";
     }
+
     @GetMapping("/mypage")
     public String mypage() {
         return "mypage";
     }
+
     @GetMapping("/order")
     public String order() {
         return "order";
     }
+
     @GetMapping("/sidebutton")
     public String sidebutton() {
         return "sidebutton";
     }
+
     @GetMapping("/checktest")
-    public String checktest(){ return "checktest"; }
+    public String checktest() {
+        return "checktest";
+    }
+
+    @GetMapping("/coffeebean")
+    public String coffee_info() {
+        return "coffeebean_info";
+    }
 }
