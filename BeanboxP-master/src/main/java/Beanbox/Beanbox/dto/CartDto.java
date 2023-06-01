805956ed1131744dee4b104798ad8ead19b76f3c
user <pdh3420@naver.com>
2023-06-01 13:14:32 +0900

    Changes

---
+++ BeanboxP-master/src/main/java/Beanbox/Beanbox/dto/CartDto.java
@@ -0,0 +1,16 @@
+package Beanbox.Beanbox.dto;
+
+import lombok.Data;
+
+
+
+    @Data
+    public class CartDto {
+        public int cart_number;
+        public String user_id;
+        public int product_number;
+        public int item_quantity;
+
+    }
+
+
