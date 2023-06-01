e82b6a38e0686f7201167683d3e4095a65b2959a
KKKJJJSSS <kj971023@naver.com>
2023-06-01 12:49:02 +0900

    230601

---
+++ BeanboxP-master/src/main/java/Beanbox/Beanbox/dto/UserDto.java
@@ -0,0 +1,11 @@
+package Beanbox.Beanbox.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    public String username;
+    public String password;
+    public String email;
+    public String number;
+}
